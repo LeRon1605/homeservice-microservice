ac4e40cdab2b20c612145255bc42172c2d8dff27
Ron Le <ronle9519@gmail.com>
2023-09-11 17:14:23 +0700

    feat: add serilog & seq

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,14 +1,20 @@
+using BuildingBlocks.Infrastructure.Serilog;
 using BuildingBlocks.Presentation.Swagger;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = ApplicationLoggerFactory.CreateSerilogLogger(builder.Configuration, "HomeAppService");
 
 builder.Services.AddControllers();
 
 builder.Services.AddSwagger("HomeAppService");
 builder.Services.AddOcelot();
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
--- Services/IAC/Presentation/IAC.API/Program.cs
+++ Services/IAC/Presentation/IAC.API/Program.cs
@@ -1,9 +1,15 @@
+using BuildingBlocks.Infrastructure.Serilog;
 using BuildingBlocks.Presentation.Swagger;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = ApplicationLoggerFactory.CreateSerilogLogger(builder.Configuration, "IdentityService");
+
 builder.Services.AddSwagger("IdentityService");
 builder.Services.AddControllers();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
