5fad45120a142332f82112bc487adaba750f6613
pp311 <phucnhphc@gmail.com>
2023-09-13 12:13:05 +0700

    feat: add docker compose

--- Services/Products/Products.Application/Commands/ProductCommands/AddProduct/AddProductCommandHandler.cs
+++ Services/Products/Products.Application/Commands/ProductCommands/AddProduct/AddProductCommandHandler.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using BuildingBlocks.Application.CQRS;
 using BuildingBlocks.Domain.Data;
+using Microsoft.Extensions.Logging;
 using Products.Application.Dtos;
 using Products.Domain.ProductAggregate;
 
@@ -11,14 +12,17 @@ public class AddProductCommandHandler : ICommandHandler<AddProductCommand, GetPr
     private readonly IUnitOfWork _unitOfWork;
     private readonly IRepository<Product> _productRepository;
     private readonly IMapper _mapper;
+    private readonly ILogger<AddProductCommandHandler> _logger;
 
     public AddProductCommandHandler(IUnitOfWork unitOfWork,
                                     IRepository<Product> productRepository,
-                                    IMapper mapper)
+                                    IMapper mapper,
+                                    ILogger<AddProductCommandHandler> logger)
     {
         _unitOfWork = unitOfWork;
         _productRepository = productRepository;
         _mapper = mapper;
+        _logger = logger;
     }
 
     public async Task<GetProductDto> Handle(AddProductCommand request,
@@ -26,9 +30,13 @@ public class AddProductCommandHandler : ICommandHandler<AddProductCommand, GetPr
     {
         var product = new Product(request.Name);
 
+        _logger.LogInformation("Adding product with name: {Name}", product.Name);
+        
         _productRepository.Add(product);
 
         await _unitOfWork.SaveChangesAsync();
+        
+        _logger.LogInformation("Product with name: {Name} added successfully", product.Name);
         
         return _mapper.Map<GetProductDto>(product); 
     }
