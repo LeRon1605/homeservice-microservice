2ed91324e27d7f6a30ea506a1b8df6508699c0a4
MaiVietQuynh <maivietquynh2001@gmail.com>
2023-09-14 13:36:46 +0700

    feat: config database user

---
+++ Services/IAC/IAC.Application/Services/Interfaces/IAuthenticateService.cs
@@ -0,0 +1,8 @@
+﻿using IAC.Application.Dtos.Authentication;
+
+namespace IAC.Application.Services.Interfaces;
+
+public interface IAuthenticateService
+{
+    Task SignUp(SignUpDto signUpDto);
+}
--- Services/IAC/IAC.Infrastructure.EfCore/Repositories/UserRepository.cs
+++ Services/IAC/IAC.Infrastructure.EfCore/Repositories/UserRepository.cs
@@ -1,8 +1,25 @@
+using IAC.Domain.Entities;
 using IAC.Domain.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace IAC.Infrastructure.EfCore.Repositories;
 
 public class UserRepository : IUserRepository
 {
-    
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public UserRepository(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+    public async Task<bool> IsPhoneExist(string phoneNumber)
+    {
+        return await _userManager.Users.AnyAsync(x => x.PhoneNumber == phoneNumber);
+    }
+
+    public async Task<bool> IsEmailExist(string? email)
+    {
+        return await _userManager.Users.AnyAsync(x => x.PhoneNumber == email);
+    }
 }
