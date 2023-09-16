2eb36e37bb04dc90f9a788c053493128227e07df
pp311 <phucnhphc@gmail.com>
2023-09-16 10:11:32 +0700

    refactor: remove redundant user field when refresh token

--- Services/IAC/IAC.Application/Dtos/Authentication/TokenDto.cs
+++ Services/IAC/IAC.Application/Dtos/Authentication/TokenDto.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using IAC.Application.Dtos.Users;
 
 namespace IAC.Application.Dtos.Authentication;
@@ -7,5 +8,6 @@ public class TokenDto
     public string AccessToken { get; set; } = null!;
     public string RefreshToken { get; set; } = null!;
     
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public UserDto? User { get; set; }
 }
