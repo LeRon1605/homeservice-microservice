e6b4663ac214023cd5b04041fc9095571ac37791
Ron Le <ronle9519@gmail.com>
2023-09-18 09:34:38 +0700

    fix: fix route for auth endpoint

--- Services/IAC/IAC.API/Controllers/AuthenticateController.cs
+++ Services/IAC/IAC.API/Controllers/AuthenticateController.cs
@@ -2,37 +2,36 @@
 using IAC.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
-namespace IAC.API.Controllers
-{
-	[Route("api/users")]
-	[ApiController]
-	public class AuthenticateController : ControllerBase
-	{
-		private readonly IAuthenticateService _authenticateService;
-        public AuthenticateController(IAuthenticateService authenticateService)
-        {
-	        _authenticateService = authenticateService;
-        }
+namespace IAC.API.Controllers;
 
-        [HttpPost("sign-up")]
-        public async Task<IActionResult> SignUpAsync([FromBody] SignUpDto signUpDto)
-        {
-	        await _authenticateService.SignUpAsync(signUpDto);
-	        return Ok();
-        }
+[Route("api/auth")]
+[ApiController]
+public class AuthenticateController : ControllerBase
+{
+	private readonly IAuthenticateService _authenticateService;
+	public AuthenticateController(IAuthenticateService authenticateService)
+	{
+		_authenticateService = authenticateService;
+	}
+
+	[HttpPost("sign-up")]
+	public async Task<IActionResult> SignUpAsync([FromBody] SignUpDto signUpDto)
+	{
+		await _authenticateService.SignUpAsync(signUpDto);
+		return Ok();
+	}
         
-        [HttpPost("log-in")]
-        public async Task<IActionResult> Login([FromBody] LoginDto logInDto)
-		{
-	        var result = await _authenticateService.LoginAsync(logInDto);
-	        return Ok(result);
-		}
+	[HttpPost("log-in")]
+	public async Task<IActionResult> Login([FromBody] LoginDto logInDto)
+	{
+		var result = await _authenticateService.LoginAsync(logInDto);
+		return Ok(result);
+	}
         
-        [HttpPost("refresh-token")]
-        public async Task<IActionResult> RefreshToken([FromBody] RefreshTokenDto refreshTokenDto)
-		{
-	        var result = await _authenticateService.RefreshTokenAsync(refreshTokenDto);
-	        return Ok(result);
-		}
-    }
+	[HttpPost("refresh-token")]
+	public async Task<IActionResult> RefreshToken([FromBody] RefreshTokenDto refreshTokenDto)
+	{
+		var result = await _authenticateService.RefreshTokenAsync(refreshTokenDto);
+		return Ok(result);
+	}
 }
