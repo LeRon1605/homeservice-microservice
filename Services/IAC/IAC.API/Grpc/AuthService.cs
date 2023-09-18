b45b5552af815d5b66baf733ab4046c0c296ca5c
Ron Le <ronle9519@gmail.com>
2023-09-18 11:10:45 +0700

    feat: add grpc get user claims endpoint

---
+++ Services/IAC/IAC.API/Grpc/AuthService.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Claims;
+using BuildingBlocks.Application.Identity;
+using Grpc.Core;
+using IAC.API.Proto;
+using IAC.Domain.Entities;
+using IAC.Domain.Exceptions.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace IAC.API.Grpc;
+
+public class AuthService : AuthProvider.AuthProviderBase
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly ICurrentUser _currentUser;
+
+    public AuthService(
+        UserManager<ApplicationUser> userManager,
+        ICurrentUser currentUser)
+    {
+        _userManager = userManager;
+        _currentUser = currentUser;
+    }
+
+    public override async Task GetClaim(Empty empty, IServerStreamWriter<ClaimResponse> responseStream, ServerCallContext context)
+    {
+        var us1er = context.GetHttpContext().User.Identity.IsAuthenticated;
+        var userId = context.GetHttpContext().User.Identity.Name;
+
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null)
+        {
+            throw new UserNotFoundException(userId);
+        }
+
+        var claims = await GetUserClaims(user);
+
+        foreach (var claim in claims)
+        {
+            await responseStream.WriteAsync(new ClaimResponse()
+            {
+                Type = claim.Type,
+                Value = claim.Value
+            });
+        }
+    }
+
+    private async Task<IEnumerable<Claim>> GetUserClaims(ApplicationUser user)
+    {
+        var claims = new List<Claim>()
+        {
+            new(ClaimTypes.NameIdentifier, user.Id),
+            new(ClaimTypes.Name, user.UserName),
+            new(ClaimTypes.Email, user.Email),
+        };
+
+        foreach (var role in await _userManager.GetRolesAsync(user))
+        {
+            claims.Add(new(ClaimTypes.Role, role));
+        }
+
+        return claims;
+    }
+}
--- Services/IAC/IAC.API/Program.cs
+++ Services/IAC/IAC.API/Program.cs
@@ -1,10 +1,13 @@
+using BuildingBlocks.Application.Identity;
 using BuildingBlocks.EventBus.Interfaces;
 using BuildingBlocks.Infrastructure.Serilog;
+using BuildingBlocks.Presentation.Authorization;
 using BuildingBlocks.Presentation.DataSeeder;
 using BuildingBlocks.Presentation.EventBus;
 using BuildingBlocks.Presentation.ExceptionHandlers;
 using BuildingBlocks.Presentation.Swagger;
 using IAC.API.Extensions;
+using IAC.API.Grpc;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 Log.Logger = ApplicationLoggerFactory.CreateSerilogLogger(builder.Configuration, "IdentityService");
 
 builder.Services.AddSwagger("IdentityService")
-                // .AddServiceCache()
+                .AddApplicationAuthentication(builder.Configuration)
                 .AddRepositories()
                 .AddMapper()
                 .AddServices()
@@ -20,21 +23,34 @@ builder.Services.AddSwagger("IdentityService")
                 .AddEventBus(builder.Configuration)
                 .AddDatabase(builder.Configuration, builder.Environment)
                 .AddIdentity(builder.Configuration, builder.Environment)
-                .AddConfiguration(builder.Configuration);
+                .AddConfiguration(builder.Configuration)
+                .AddGrpcReflection()
+                .AddGrpc();
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<ICurrentUser, CurrentUser>();
 
 builder.Host.UseSerilog();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
+
 app.UseApplicationExceptionHandler();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 var eventBus = app.Services.GetRequiredService<IEventBus>();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.MapGrpcService<AuthService>();
 app.MapControllers();
 
 await app.ApplyMigrationAsync(app.Logger);
