7ccad63752c9dcaa48c4d5feb62becfde4e5a203
pp311 <phucnhphc@gmail.com>
2023-09-20 11:51:33 +0700

    refactor: login return fullname

--- Services/IAC/IAC.Application/Dtos/Users/UserDto.cs
+++ Services/IAC/IAC.Application/Dtos/Users/UserDto.cs
@@ -3,6 +3,5 @@ namespace IAC.Application.Dtos.Users;
 public class UserDto
 {
     public Guid Id { get; set; }
-    public string FirstName { get; set; } = default!;
-    public string LastName { get; set; } = default!;
+    public string FullName { get; set; } = default!;
 }
