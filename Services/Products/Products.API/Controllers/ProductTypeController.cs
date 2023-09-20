84f1e7513d8a4b9dec6bfa959d721cc9bd5b9abe
pp311 <phucnhphc@gmail.com>
2023-09-20 12:00:26 +0700

    refactor: move get product types, groups and units to it own controller

---
+++ Services/Products/Products.API/Controllers/ProductTypeController.cs
@@ -0,0 +1,28 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Products.Application.Dtos;
+using Products.Application.Queries.ProductQuery.GetAllProductType;
+
+namespace Products.API.Controllers;
+
+[ApiController]
+[Route("api/product-types")]
+public class ProductTypeController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ProductTypeController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<ProductTypeDto>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetProductTypesAsync([FromQuery] GetAllProductTypeQuery query)
+    {
+        var productTypes = await _mediator.Send(query);
+        return Ok(productTypes);
+    }
+    
+    
+}
---
+++ Services/Products/Products.API/Controllers/ProductUnitController.cs
@@ -0,0 +1,26 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Products.Application.Dtos;
+using Products.Application.Queries.ProductQuery.GetAllProductUnit;
+
+namespace Products.API.Controllers;
+
+[ApiController]
+[Route("api/product-units")]
+public class ProductUnitController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ProductUnitController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<ProductUnitDto>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetProductUnitsAsync([FromQuery] GetAllProductUnitQuery query)
+    {
+        var productUnits = await _mediator.Send(query);
+        return Ok(productUnits);
+    }
+}
