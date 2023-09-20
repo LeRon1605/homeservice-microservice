fb1f7650645831d18449f43e9559e10ea787f1a2
Ron Le <ronle9519@gmail.com>
2023-09-20 10:23:12 +0700

    fix: fix material data seeder

--- Services/Products/Products.Application/Seeders/MaterialDataSeeder.cs
+++ Services/Products/Products.Application/Seeders/MaterialDataSeeder.cs
@@ -3,6 +3,7 @@ using BuildingBlocks.Application.Seeder;
 using BuildingBlocks.Domain.Data;
 using Microsoft.Extensions.Logging;
 using Products.Domain.MaterialAggregate;
+using Products.Domain.MaterialAggregate.DomainServices;
 using Products.Domain.ProductTypeAggregate;
 using Products.Domain.ProductUnitAggregate;
 
@@ -13,10 +14,12 @@ public class MaterialDataSeeder: IDataSeeder
     private readonly IRepository<Material> _materialRepository;
     private readonly IReadOnlyRepository<ProductType> _productTypeRepository;
     private readonly IReadOnlyRepository<ProductUnit> _productUnitRepository;
+    private readonly IMaterialDomainService _materialDomainService;
     private readonly IUnitOfWork _unitOfWork;
-    private ILogger<MaterialDataSeeder> _logger;
+    private readonly ILogger<MaterialDataSeeder> _logger;
 
     public MaterialDataSeeder(IRepository<Material> materialRepository, 
+        IMaterialDomainService materialDomainService,
         IUnitOfWork unitOfWork,
         ILogger<MaterialDataSeeder> logger, 
         IReadOnlyRepository<ProductType> productTypeRepository,
@@ -27,6 +30,7 @@ public class MaterialDataSeeder: IDataSeeder
         _logger = logger;
         _productTypeRepository = productTypeRepository;
         _productUnitRepository = productUnitRepository;
+        _materialDomainService = materialDomainService;
     }
 
     public async Task SeedAsync()
@@ -40,31 +44,30 @@ public class MaterialDataSeeder: IDataSeeder
         var productUnits = await _productUnitRepository.GetAllAsync();
         var productTypes = await _productTypeRepository.GetAllAsync();
         
-        SeedMaterials(productUnits.ToList(),productTypes.ToList());
+        await SeedMaterialsAsync(productUnits.ToList(),productTypes.ToList());
         await _unitOfWork.SaveChangesAsync();
         
         _logger.LogInformation("Material data seeded successfully!");
     }
 
     
-    private void SeedMaterials(
+    private async Task SeedMaterialsAsync(
         IReadOnlyList<ProductUnit> productUnits, 
-        IReadOnlyList<ProductType> productTypes
-        )
+        IReadOnlyList<ProductType> productTypes)
     {
         var faker = new Faker();
         var random = new Random();
         for (var i = 0; i < 9; i++)
         {
-            _materialRepository.Add(new Material(
+            var material = await _materialDomainService.CreateAsync(
                 faker.Random.AlphaNumeric(6),
                 faker.Commerce.ProductMaterial(),
                 productTypes[random.Next(0, productTypes.Count)].Id,
                 productUnits[random.Next(0, productTypes.Count)].Id,
                 faker.Random.Decimal(1, 100),
                 faker.Random.Decimal(1, 100),
-                faker.Random.Bool(0.1f)
-            ));
+                faker.Random.Bool(0.1f));
+            _materialRepository.Add(material);
         }
     }
 }
