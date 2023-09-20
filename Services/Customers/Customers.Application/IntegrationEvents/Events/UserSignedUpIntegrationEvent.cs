be64d97ec0f1f8f5371c5becd5ed3bc197787db6
pp311 <phucnhphc@gmail.com>
2023-09-20 11:48:23 +0700

    feat: add new customer when a user signed up

---
+++ Services/Customers/Customers.Application/IntegrationEvents/Events/UserSignedUpIntegrationEvent.cs
@@ -0,0 +1,23 @@
+﻿using BuildingBlocks.Application.IntegrationEvent;
+
+namespace Customers.Application.IntegrationEvents.Events;
+
+public record UserSignedUpIntegrationEvent : IntegrationEvent
+{
+    public Guid UserId { get; set; }
+    public string FullName { get; set; }
+    public string? Email { get; set; }
+    public string Phone { get; set; }
+
+    public UserSignedUpIntegrationEvent(Guid id, 
+                                      string fullName,
+                                      string? email,
+                                      string phone)
+    {
+        UserId = id;
+        FullName = fullName;
+        Email = email;
+        Phone = phone;
+    }
+}
+
