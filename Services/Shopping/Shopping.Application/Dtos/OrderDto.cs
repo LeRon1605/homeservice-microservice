4f50e48f87b3a7d0b4c5e7f14a7196086f4e06b6
MaiVietQuynh <maivietquynh2001@gmail.com>
2023-09-26 14:25:15 +0700

    feat: update OrderDto

---
+++ Services/Shopping/Shopping.Application/Dtos/OrderDto.cs
@@ -0,0 +1,17 @@
+﻿using Shopping.Domain.OrderAggregate;
+
+namespace Shopping.Application.Dtos;
+
+public class OrderDto
+{
+    public Guid Id { get; private set; }
+    public string OrderNo { get; private set; }
+    public string ContactName { get; private set; }
+    public Guid BuyerId { get; private set; }
+    public string PhoneNumber { get; private set; }
+    public string? EmailAddress { get; private set; }
+    public decimal OrderValue { get; private set; }
+    public DateTime PlacedDate { get; private set; }
+    public OrderStatus Status { get; private set; }
+
+}
