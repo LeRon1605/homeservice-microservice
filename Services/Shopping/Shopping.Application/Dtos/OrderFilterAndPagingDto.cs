b956a1d9cca5c70e80e0ad4898bd0128769d8ce3
Ron Le <ronle9519@gmail.com>
2023-09-28 08:42:04 +0700

    fix: mapping string to enum

--- Services/Shopping/Shopping.Application/Dtos/OrderFilterAndPagingDto.cs
+++ Services/Shopping/Shopping.Application/Dtos/OrderFilterAndPagingDto.cs
@@ -1,9 +1,11 @@
-﻿using BuildingBlocks.Application.Dtos;
+﻿using System.Text.Json.Serialization;
+using BuildingBlocks.Application.Dtos;
 using Shopping.Domain.OrderAggregate;
 
 namespace Shopping.Application.Dtos;
 
 public class OrderFilterAndPagingDto : PagingParameters
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public OrderStatus? Status { get; set; }
 }
