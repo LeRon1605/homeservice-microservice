7999c3474dd2bf0fe026a8591920c834a9379a41
pp311 <phucnhphc@gmail.com>
2023-10-13 23:34:53 +0700

    fix(contract): fix date time compare when get list

--- Services/Contracts/Contracts.Domain/ContractAggregate/Specifications/GetContractsSpecification.cs
+++ Services/Contracts/Contracts.Domain/ContractAggregate/Specifications/GetContractsSpecification.cs
@@ -27,15 +27,25 @@ public class GetContractsSpecification : Specification<Contract>
         if (statusList != null)
             AddFilter(x => statusList.Contains(x.Status));
 
-        if (dateType.HasValue)
+        if (dateType.HasValue && fromDate.HasValue && toDate.HasValue)
         {
+            fromDate = fromDate.Value.Date;
+            toDate = toDate.Value.Date;
             Expression<Func<Contract, bool>> dateFilter = dateType.Value switch
             {
-                DateFilterType.DateQuoted => x => x.QuotedAt >= fromDate && x.QuotedAt <= toDate,
-                DateFilterType.DateSold => x => x.SoldAt >= fromDate && x.SoldAt <= toDate,
-                DateFilterType.InvoiceDate => x => x.InvoiceDate >= fromDate && x.InvoiceDate <= toDate,
-                DateFilterType.ActualInstallDate => x => x.ActualInstallationDate >= fromDate && x.ActualInstallationDate <= toDate,
-                DateFilterType.EstimatedInstallDate => x => x.EstimatedInstallationDate >= fromDate && x.EstimatedInstallationDate <= toDate,
+                DateFilterType.DateQuoted => x => x.QuotedAt.Date >= fromDate && x.QuotedAt.Date <= toDate,
+                DateFilterType.DateSold => x => x.SoldAt.HasValue 
+                                                && x.SoldAt.Value.Date >= fromDate 
+                                                && x.SoldAt.Value.Date <= toDate,
+                DateFilterType.InvoiceDate => x => x.InvoiceDate.HasValue 
+                                                   && x.InvoiceDate.Value.Date >= fromDate 
+                                                   && x.InvoiceDate.Value.Date <= toDate,
+                DateFilterType.ActualInstallDate => x => x.ActualInstallationDate.HasValue 
+                                                         && x.ActualInstallationDate.Value.Date >= fromDate 
+                                                         && x.ActualInstallationDate.Value.Date <= toDate,
+                DateFilterType.EstimatedInstallDate => x => x.EstimatedInstallationDate.HasValue 
+                                                            && x.EstimatedInstallationDate.Value.Date >= fromDate 
+                                                            && x.EstimatedInstallationDate.Value.Date <= toDate,
                 _ => x => false
             };
 
